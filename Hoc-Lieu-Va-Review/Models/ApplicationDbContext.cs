a44a0792a20d5b3ed40102e29fd430e93fe00759
Minh Quan <quanchm2208@gmail.com>
2026-02-12 08:55:16 +0700

    Connect to Database

---
+++ Hoc-Lieu-Va-Review/Models/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Hoc_Lieu_Va_Review.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        // Khai báo các bảng (DbSet) của bạn ở đây, ví dụ:
+        // public DbSet<Product> Products { get; set; }
+    }
+}
--- Hoc-Lieu-Va-Review/Program.cs
+++ Hoc-Lieu-Va-Review/Program.cs
@@ -1,4 +1,12 @@
+
+using Microsoft.EntityFrameworkCore;
+using Hoc_Lieu_Va_Review.Models;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(connectionString));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
