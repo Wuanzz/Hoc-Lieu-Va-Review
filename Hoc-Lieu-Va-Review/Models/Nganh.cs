dbdf2e55c94d75d6ae7f30b76bc401fd6cdbadae
Minh Quan <quanchm2208@gmail.com>
2026-03-01 10:02:06 +0700

    Add-Migration and Update-Database for 8Models

---
+++ Hoc-Lieu-Va-Review/Models/Nganh.cs
@@ -0,0 +1,26 @@
+﻿using Hoc_Lieu_Va_Review.Models;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Hoc_Lieu_Va_Review.Models
+{
+    [Table("Nganh")]
+    public class Nganh
+    {
+        [Key]
+        public int NganhID { get; set; }
+
+        [Required]
+        [StringLength(255)]
+        public string TenNganh { get; set; }
+
+        public string MoTa { get; set; }
+
+        [ForeignKey("Khoa")]
+        public int KhoaID { get; set; }
+
+        // Navigation properties
+        public virtual Khoa Khoa { get; set; }
+        public virtual ICollection<HocPhan> HocPhans { get; set; } = new List<HocPhan>();
+    }
+}
