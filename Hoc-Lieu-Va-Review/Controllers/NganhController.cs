4a1063c5e9ae6f52cc4c21c5a0d8924f370b5cf1
HuyPham <huy4200pc@gmail.com>
2026-03-02 12:38:38 +0700

    Add major management create

--- Hoc-Lieu-Va-Review/Controllers/NganhController.cs
+++ Hoc-Lieu-Va-Review/Controllers/NganhController.cs
@@ -45,5 +45,33 @@ namespace Hoc_Lieu_Va_Review.Controllers
 
             return View(danhSachNganh);
         }
+        // [GET] Hiển thị form Thêm mới
+        [HttpGet]
+        public IActionResult Create()
+        {
+            // Tạo một danh sách thả xuống (SelectList) chứa các Khoa để chọn
+            // "KhoaID" là giá trị ẩn (value), "TenKhoa" là chữ hiển thị ra ngoài (text)
+            ViewData["KhoaID"] = new SelectList(_context.Khoas, "KhoaID", "TenKhoa");
+            return View();
+        }
+
+        // [POST] Lưu Ngành mới vào Database
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("TenNganh,MoTa,KhoaID")] Nganh nganh)
+        {
+            // Yêu cầu hệ thống không bắt lỗi thiếu dữ liệu của object Khoa
+            ModelState.Remove("Khoa");
+
+            if (ModelState.IsValid)
+            {
+                _context.Add(nganh);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            // Nếu lỗi, phải nạp lại danh sách Khoa cho dropdown kẻo bị lỗi giao diện
+            ViewData["KhoaID"] = new SelectList(_context.Khoas, "KhoaID", "TenKhoa", nganh.KhoaID);
+            return View(nganh);
+        }
     }
 }
