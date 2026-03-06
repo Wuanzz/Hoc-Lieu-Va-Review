270f8edba1f9c6f63187225f3bc518db388f7d9c
Supzxje <leducanhbvh0@gmail.com>
2026-03-06 19:15:56 +0700

    Approve the report

---
+++ Hoc-Lieu-Va-Review/Views/TaiLieu/Detail.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hoc_Lieu_Va_Review.Views.TaiLieu
+{
+    public class DetailModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
