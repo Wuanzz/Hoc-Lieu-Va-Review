96fb5fdf49c8e23a814443874b89358ce16eaade
Minh Quan <quanchm2208@gmail.com>
2026-05-13 16:15:24 +0700

    Statistics Dashboard

--- Hoc-Lieu-Va-Review/Controllers/AccountController.cs
+++ Hoc-Lieu-Va-Review/Controllers/AccountController.cs
@@ -24,7 +24,7 @@ namespace Hoc_Lieu_Va_Review.Controllers
             {
                 if (User.IsInRole("Admin"))
                 {
-                    return RedirectToAction("Index", "Khoa", new { area = "Admin" });
+                    return RedirectToAction("Index", "Home", new { area = "Admin" });
                 }
                 else if (User.IsInRole("GiangVien"))
                 {
@@ -67,8 +67,8 @@ namespace Hoc_Lieu_Va_Review.Controllers
                     // --- PHÂN LUỒNG ĐIỀU HƯỚNG THEO VAI TRÒ ---
                     if (user.VaiTro == "Admin")
                     {
-                        // Admin vào thẳng trang Quản lý Khoa
-                        return RedirectToAction("Index", "Khoa", new { area = "Admin" });
+                        // Admin vào thẳng trang Dashboard
+                        return RedirectToAction("Index", "Home", new { area = "Admin" });
                     }
                     else if (user.VaiTro == "GiangVien")
                     {
